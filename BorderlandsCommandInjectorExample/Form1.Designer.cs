f73f66e54c52a3f7475ae12cd701850d28de9f3f
c0dycode <schulten_705@hotmail.com>
2018-08-19 14:47:40 +0200

    Initial push of Exampleproject files and compiled CommandInjector DLL Plugins

--- BorderlandsCommandInjectorExample/Form1.Designer.cs
+++ BorderlandsCommandInjectorExample/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textBoxCommand = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rtbEvents = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(272, 20);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // textBoxCommand
+            // 
+            this.textBoxCommand.Location = new System.Drawing.Point(13, 22);
+            this.textBoxCommand.Name = "textBoxCommand";
+            this.textBoxCommand.Size = new System.Drawing.Size(253, 20);
+            this.textBoxCommand.TabIndex = 1;
+            this.textBoxCommand.TextChanged += new System.EventHandler(this.textBoxCommand_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 49);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(965, 746);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // rtbEvents
+            // 
+            this.rtbEvents.Location = new System.Drawing.Point(12, 801);
+            this.rtbEvents.Name = "rtbEvents";
+            this.rtbEvents.Size = new System.Drawing.Size(965, 81);
+            this.rtbEvents.TabIndex = 4;
+            this.rtbEvents.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(989, 894);
+            this.Controls.Add(this.rtbEvents);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBoxCommand);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Borderlands 2 Command Injector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textBoxCommand;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox rtbEvents;
     }
 }
 
--- BorderlandsCommandInjectorExample/Form1.cs
+++ BorderlandsCommandInjectorExample/Form1.cs
@@ -1,20 +1,176 @@
 ﻿using System;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Threading;
 
 namespace BorderlandsCommandInjectorExample
 {
     public partial class Form1 : Form
     {
+        private NamedPipeClientStream pipe;
+        private NamedPipeClientStream eventPipe;
+        private List<string> responseList = new List<string>();
+        private List<string> eventList = new List<string>();
+        private BackgroundWorker BW = new BackgroundWorker();
+
+        private string commandText { get; set; }
+
         public Form1()
         {
             InitializeComponent();
+
+            // Create BackgroundWorker to create a seperate eventPipe
+            BW.DoWork += BW_DoWork;
+            BW.RunWorkerCompleted += BW_RunWorkerCompleted;
+            BW.RunWorkerAsync();
+        }
+
+        private void BW_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // Run EventListener again
+            BW.RunWorkerAsync();
+        }
+
+        private void BW_DoWork(object sender, DoWorkEventArgs e)
+        {
+            eventPipe = new NamedPipeClientStream(".", "BLCommandInjectorEvents", PipeDirection.InOut);
+            while (!eventPipe.IsConnected)
+            {
+                try
+                {
+                    eventPipe.Connect();
+                }
+                catch (Exception ex) { }
+            }
+
+            // Create the WaitHandle to wait for the NamedEvent the DLL fires
+            WaitHandle waitEventHandle = new EventWaitHandle(false, EventResetMode.ManualReset, "GlobalReadLevelTransitionEvent");
+            try
+            {
+                // Wait until CommandInjector fired the namedevent
+                waitEventHandle.WaitOne();
+                if (eventPipe.IsConnected)
+                {
+                    // Set Pipemode to MessageMode
+                    eventPipe.ReadMode = PipeTransmissionMode.Message;
+                    var streamReader = new StreamReader(eventPipe);
+
+                    string eventCheck = "";
+                    eventList.Clear();
+
+                    // While there is still data to read, add the current line to the List
+                    while ((eventCheck = streamReader.ReadLine()) != null)
+                    {
+                        if (eventCheck != string.Empty)
+                            eventList.Add(eventCheck);
+                    }
+
+                    streamReader.Close();
+                    streamReader.Dispose();
+
+                    // Update UI from Non-UI-Thread
+                    if (eventList.Count > 0)
+                    {
+                        this.rtbEvents.Invoke((MethodInvoker)delegate
+                        {
+                            this.rtbEvents.AppendText(eventList[0] + "\r\n");
+                        });
+                    }
+                }
+            }
+            catch (IOException ioexc)
+            {
+                // Ignore "Pipe Interrupted/Broken" exception
+                if (ioexc.HResult.ToString("X") != "800700E9")
+                {
+                    //Debugger.Break();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBoxCommand_TextChanged(object sender, EventArgs e)
+        {
+            commandText = (sender as TextBox)?.Text;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            pipe = new NamedPipeClientStream(".", "BLCommandInjector", PipeDirection.InOut);
+
+            if (!pipe.IsConnected)
+                pipe.Connect();
+
+            if (pipe.IsConnected)
+            {
+                try
+                {
+                    // Create a new StreaWriter which disposes itself after usage
+                    using (var sw = new StreamWriter(pipe))
+                    {
+                        // Don't send empty commands to the pipe, ya doofus!
+                        if (!string.IsNullOrWhiteSpace(commandText))
+                        {
+                            sw.WriteLine(commandText);
+
+                            // Flush the Buffer, so the Pipe gets a signal to make sure it's read from completely!!
+                            sw.Flush();
+                        }
+
+                        //
+                        // If you want to get a response, read it here, otherwise, server will close the connection!
+                        // 
+
+                        // Set Pipemode to MessageMode
+                        pipe.ReadMode = PipeTransmissionMode.Message;
+
+                        // No "using" this time, since otherwise we get an exception, since the pipe closes automatically when the Stream closes/disposes
+                        var reader = new StreamReader(pipe);
+
+                        string check = "";
+
+                        // Create a new empty List which will hold our console output line by line.
+                        responseList.Clear();
+
+                        // While there is still data to read, add the current line to the List
+                        while ((check = reader.ReadLine()) != null)
+                        {
+                            if (check != string.Empty)
+                                responseList.Add(check);
+                        }
+
+                        // Refresh the DataSource in the UI
+                        if (responseList.Count > 0)
+                        {
+                            //this.rtbEvents.DataSource = null;
+                            //this.listBoxEvents.DataSource = responseList;
+                        }
+                        richTextBox1.ResetText();
+                        foreach (var line in responseList)
+                        {
+                            richTextBox1.AppendText(line + "\r\n");
+                        }
+                    }
+                }
+                catch (IOException ioexc)
+                {
+                    // Ignore "Pipe Interrupted/Broken" exception
+                    if (ioexc.HResult.ToString("X") != "80131620")
+                    {
+                        //Debugger.Break();
+                    }
+                }
+            }
+            // Clear the Command TextBox in the UI
+            this.textBoxCommand.Text = "";
         }
     }
 }
